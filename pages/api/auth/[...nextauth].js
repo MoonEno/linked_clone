d97aabf65fd4ecf61f544a06974c2184b95424bc
ihmoon <ihmoon>
2022-11-21 17:52:51 +0900

    환경설정 및 next/auth추가

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+
+export default NextAuth ({
+    // configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOGOLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        // add more providers here! kakao, naver, facebook ETC...
+    ],
+    // use JWT
+    secret: process.env.JWT_SECRET,
+    // use DB adapter
+    adapter: MongoDBAdapter(clientPromise),
+    // after url Auth Login & Logout
+    pages: {
+        signIn : "/home"
+    },
+    // session managed two-ways, jwt or database 
+    // default > adapter(database)
+    session: {
+        strategy: "jwt",
+    },
+    debug: true
+});
---
+++ pages/test.js
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import React from 'react';
+
+
+function test() {
+  
+  return (
+    <div className='container'>
+      <div className='item'>
+        <p>사랑이 이젠 잊으라네요 붙잡아도 소용없대요 시간이 지나가면 편해진다고 잊으래요 내 맘 가져놓고</p>
+      </div>
+      <div className='item'>
+        <p>내 사랑 지켜만 봐도 안될까요</p>
+      </div>
+      <div className='item'>
+        <p>
+          아파져도 난 그대를 사랑해요<br/>
+          이렇게 나 영원히 삶이 다해도<br/>
+          나 그댈 위해 늘 기도 할거에요
+        </p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default test;
